228f5a139167477c06370c01118778c0cfba371b
Azure-Pipelines libCZI Documentation builder <nobody@h-quer.de>
2020-10-06 21:38:42 +0000

    Updating with latest documentation build

---
+++ Doxygen/html/navtreedata.js
@@ -0,0 +1,49 @@
+var NAVTREE =
+[
+  [ "libCZI", "index.html", [
+    [ "libCZI Documentation", "index.html", null ],
+    [ "Building libCZI", "buildinglib_c_z_i.html", null ],
+    [ "Image Document Concept", "imagedocumentconcept.html", null ],
+    [ "using libCZI", "using_naczirlib.html", null ],
+    [ "Accessors", "accessors.html", null ],
+    [ "Multi-channel-composition", "multichannelcomposition.html", null ],
+    [ "CZIcmd Documentation", "naczircmd.html", null ],
+    [ "3rd party software", "_3rdpartysoftware.html", null ],
+    [ "Todos", "todos.html", null ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_3rdpartysoftware.html",
+"classlib_c_z_i_1_1_scoped_bitmap_locker.html#a3d5e197884453daaa5c6252a6dd19e9e",
+"structlib_c_z_i_1_1_scaling_info.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ Doxygen/html/search/classes_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['bitmaplockinfo',['BitmapLockInfo',['../structlib_c_z_i_1_1_bitmap_lock_info.html',1,'libCZI']]],
+  ['boundingboxes',['BoundingBoxes',['../structlib_c_z_i_1_1_bounding_boxes.html',1,'libCZI']]],
+  ['buildinformation',['BuildInformation',['../structlib_c_z_i_1_1_build_information.html',1,'libCZI']]]
+];
