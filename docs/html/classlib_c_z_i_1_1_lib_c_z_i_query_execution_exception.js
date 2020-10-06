0fda5c0e8d39f7ea8cab453ba000a33d7b716c8c
Azure-Pipelines libCZI Documentation builder <nobody@h-quer.de>
2020-10-06 21:45:29 +0000

    Updating with latest documentation build

---
+++ docs/html/classlib_c_z_i_1_1_lib_c_z_i_query_execution_exception.js
@@ -0,0 +1,10 @@
+var classlib_c_z_i_1_1_lib_c_z_i_query_execution_exception =
+[
+    [ "ErrorType", "classlib_c_z_i_1_1_lib_c_z_i_query_execution_exception.html#a7420fe8895248c44d16cf34f23f0c6a2", [
+      [ "NonExistentDimension", "classlib_c_z_i_1_1_lib_c_z_i_query_execution_exception.html#a7420fe8895248c44d16cf34f23f0c6a2ad7b33b6112c42abfa0dd07aca1cbfd3b", null ],
+      [ "InternalError", "classlib_c_z_i_1_1_lib_c_z_i_query_execution_exception.html#a7420fe8895248c44d16cf34f23f0c6a2a8462b58246e70e5c83e5b939a9332cb5", null ]
+    ] ],
+    [ "LibCZIQueryExecutionException", "classlib_c_z_i_1_1_lib_c_z_i_query_execution_exception.html#a9d19dc34e051c17ae74906794b72107f", null ],
+    [ "LibCZIQueryExecutionException", "classlib_c_z_i_1_1_lib_c_z_i_query_execution_exception.html#a15b965e779ae464f4d2b5c1e7948db9a", null ],
+    [ "GetErrorType", "classlib_c_z_i_1_1_lib_c_z_i_query_execution_exception.html#a6ac7e255c61dae5fc104f0ae54d10513", null ]
+];
---
+++ docs/html/classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.js
@@ -0,0 +1,13 @@
+var classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception =
+[
+    [ "ErrorType", "classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.html#adf5a11c2415ec7c92d95365fa5af2fde", [
+      [ "SyntaxError", "classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.html#adf5a11c2415ec7c92d95365fa5af2fdeaece592fd242f4ba674cc9da539a625a7", null ],
+      [ "UnbalancedParenthesis", "classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.html#adf5a11c2415ec7c92d95365fa5af2fdea25a7407c24782c96b1bd15bc93680067", null ],
+      [ "IllformedExpression", "classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.html#adf5a11c2415ec7c92d95365fa5af2fdeab78b1f9c4bc2fc9060a5a250bee19c0c", null ],
+      [ "InvalidNumberFormat", "classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.html#adf5a11c2415ec7c92d95365fa5af2fdea225b4f0d4e2f40f443a22ce54ff7f493", null ]
+    ] ],
+    [ "LibCZIQueryParseException", "classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.html#a9c01ed75a7d690006bae37444a08a2a7", null ],
+    [ "LibCZIQueryParseException", "classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.html#a93adeb0fad20a3c8c4ea7ba68e49d127", null ],
+    [ "GetErrorType", "classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.html#a22088facd6fc204fcaf44867bfaeb8bc", null ],
+    [ "GetPositionAfterWhichParserErrorOccurred", "classlib_c_z_i_1_1_lib_c_z_i_query_parse_exception.html#ae880a97ae7923437190a968d5865b86e", null ]
+];
