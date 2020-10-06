2956e3da43a6bf302474767d478fbb3e6344a1dc
Azure-Pipelines libCZI Documentation builder <nobody@h-quer.de>
2020-10-06 22:07:35 +0000

    Updating with latest documentation build

---
+++ docs/html/classlib_c_z_i_1_1_i_query_condition.js
@@ -0,0 +1,7 @@
+var classlib_c_z_i_1_1_i_query_condition =
+[
+    [ "IQueryCondition", "classlib_c_z_i_1_1_i_query_condition.html#a4ea41bf14c48a851887a002e74866c39", null ],
+    [ "IQueryCondition", "classlib_c_z_i_1_1_i_query_condition.html#ad1edfba1587cf0755f5323acb1678202", null ],
+    [ "~IQueryCondition", "classlib_c_z_i_1_1_i_query_condition.html#a798757ed30de6ed6c72f3c826b0f2943", null ],
+    [ "operator=", "classlib_c_z_i_1_1_i_query_condition.html#a46b97faf73648229efc175632c24c186", null ]
+];
---
+++ docs/html/structlib_c_z_i_1_1_query_options.js
@@ -0,0 +1,10 @@
+var structlib_c_z_i_1_1_query_options =
+[
+    [ "HandlingOfNonExistentDimensions", "structlib_c_z_i_1_1_query_options.html#ae9ed8c23d9b03f1d146d9527b41c75fa", [
+      [ "EvaluateToTrue", "structlib_c_z_i_1_1_query_options.html#ae9ed8c23d9b03f1d146d9527b41c75faa8117d5f936be083cdaf889d9f142cc84", null ],
+      [ "EvaluateToFalse", "structlib_c_z_i_1_1_query_options.html#ae9ed8c23d9b03f1d146d9527b41c75faaea87c3a6a5273c2066b1f4145e46f42b", null ],
+      [ "Error", "structlib_c_z_i_1_1_query_options.html#ae9ed8c23d9b03f1d146d9527b41c75faa902b0d55fddef6f8d651fe1035b7d4bd", null ]
+    ] ],
+    [ "SetDefault", "structlib_c_z_i_1_1_query_options.html#aadf6570c2717cac879fc54c2f31fd079", null ],
+    [ "handlingNonExistentDimensions", "structlib_c_z_i_1_1_query_options.html#a5fa08bd9612f2e676881498f93b03981", null ]
+];
