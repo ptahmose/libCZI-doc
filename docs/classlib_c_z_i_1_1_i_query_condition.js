650cd79bda25326374c2d4030c9eada83c543c17
Azure-Pipelines libCZI Documentation builder <nobody@h-quer.de>
2020-10-08 22:45:23 +0000

    Updating with latest documentation build (170)

--- docs/classlib_c_z_i_1_1_i_query_condition.js
+++ docs/classlib_c_z_i_1_1_i_query_condition.js
@@ -1,7 +1,7 @@
 var classlib_c_z_i_1_1_i_query_condition =
 [
-    [ "IQueryCondition", "classlib_c_z_i_1_1_i_query_condition.html#a4ea41bf14c48a851887a002e74866c39", null ],
+    [ "IQueryCondition", "classlib_c_z_i_1_1_i_query_condition.html#ae24fad9c1de6914bc1906e7c085516e2", null ],
     [ "IQueryCondition", "classlib_c_z_i_1_1_i_query_condition.html#ad1edfba1587cf0755f5323acb1678202", null ],
-    [ "~IQueryCondition", "classlib_c_z_i_1_1_i_query_condition.html#a798757ed30de6ed6c72f3c826b0f2943", null ],
+    [ "~IQueryCondition", "classlib_c_z_i_1_1_i_query_condition.html#a5888ae111cc99f208e4ad3900a0950b0", null ],
     [ "operator=", "classlib_c_z_i_1_1_i_query_condition.html#a46b97faf73648229efc175632c24c186", null ]
 ];
--- docs/search/enumvalues_4.js
+++ docs/search/enumvalues_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['error',['Error',['../structlib_c_z_i_1_1_query_options.html#ae9ed8c23d9b03f1d146d9527b41c75faa902b0d55fddef6f8d651fe1035b7d4bd',1,'libCZI::QueryOptions']]],
   ['evaluatetofalse',['EvaluateToFalse',['../structlib_c_z_i_1_1_query_options.html#ae9ed8c23d9b03f1d146d9527b41c75faaea87c3a6a5273c2066b1f4145e46f42b',1,'libCZI::QueryOptions']]],
   ['evaluatetotrue',['EvaluateToTrue',['../structlib_c_z_i_1_1_query_options.html#ae9ed8c23d9b03f1d146d9527b41c75faa8117d5f936be083cdaf889d9f142cc84',1,'libCZI::QueryOptions']]]
 ];
